601aeaeeaba8c0fcb9f9ecbc80dfefd610d92845
Yuouzz <111175283+Yuouzz@users.noreply.github.com>
2023-01-18 17:39:32 +0800

    Update 1.c

--- 1.c
+++ 1.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 int main()
 {
-printf('BMSConverter v0.0 by Yuouzz/nUnder construction now/n');
+printf('BMSConverter v0.0 by Yuouzz\nUnder construction now\nIt is will be a program like wav2ogg');
 return 0;
 }
